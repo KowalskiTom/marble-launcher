95e3000d29a5d4a35633c4664e63af5f7d230bc2
KowalskiTom <kowalski.tom@gmail.com>
2026-03-14 18:44:55 -0400

    Render all parts to STL files

---
+++ render_parts.bat
@@ -0,0 +1,29 @@
+@echo off
+rem Script to render all parts of the marble launcher to STL files
+rem Requires OpenSCAD to be installed and accessible in PATH
+
+set SCAD_FILE=marble-launcher.scad
+set RENDER_DIR=renders
+set OPENSCAD="C:\Program Files\OpenSCAD\openscad.exe"
+
+rem Create renders directory if it doesn't exist
+if not exist "%RENDER_DIR%" (
+    mkdir "%RENDER_DIR%"
+    echo Created directory %RENDER_DIR%
+)
+
+rem Define parts to render: mode -> output filename
+rem We'll render housing, lever, piston, button, assembly, layout
+set PARTS=housing lever piston button assembly layout
+
+for %%M in (%PARTS%) do (
+    echo Rendering %%M...
+    %OPENSCAD% -o "%RENDER_DIR%\marble-launcher-v1-%%M.stl" -D "render_mode=\"%%M\"" "%SCAD_FILE%"
+    if errorlevel 1 (
+        echo Error rendering %%M. Make sure OpenSCAD is installed and in PATH.
+        exit /b 1
+    )
+    echo Rendered %%M to marble-launcher-v1-%%M.stl
+)
+
+echo All parts rendered successfully.
